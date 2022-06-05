f4f19134ff13d25a55b4c7d152f63b1ed3a688dd
kvv-9209 <kvv-9209@mail.ru>
2022-06-05 16:22:05 +0300

    Initial commit

---
+++ src/main/java/ru/gb/shopthymeleaf/ShopThymeleafApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.shopthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopThymeleafApplication.class, args);
+    }
+
+}
